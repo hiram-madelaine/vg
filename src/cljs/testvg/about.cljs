46dee499591ce864890df654cb6563a292d245fa
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-07-13 13:26:52 +0200

    FIX typos

--- src/cljs/testvg/about.cljs
+++ src/cljs/testvg/about.cljs
@@ -9,20 +9,19 @@
     [:h3 "Om, Reagent and State"]
     "I am an Om person and it was a lot of fun to dive into Reagent !
     At first the two libraries seem very different but after a few hours I felt (quite) comfortable with Reagent.
-    The biggest hurdle for me has been the transiton form cursors to reagent/atom.
+    The biggest hurdle for me has been the transition form cursors to reagent/atom.
     Om is more directive and in Reagent you have more freedom. The problem I faced was to grok the best practices.
     I am used to have a single global state, but in the examples I saw a multiplication of different pieces of state.
     I don't like to def a piece of state and reference it in the body of a function several lines down.
-    I had rather pass explicitely the state as an argument"]
+    I had rather pass explicitly the state as an argument"]
 
    [:article
-    [:h3 "The way I approched the problem"]
+    [:h3 "The way I approached the problem"]
     "Starting from Test3 each solution is based on the previous test.
     I did not copy and paste code because I have been genetically modified to prevent it.
     I tried to compose the previous building blocks to come up with a solution for the next step.
-    In Test3 I define a \"startable-component\" that can be controled via a core.async channel.
-    This generic component is used to define a \"startable-clock-compoent\".
-    In the \"Vigiglobe API\" exercice the same generic component is used to start and stop the live data update. "]
+    In Test3 I define a \"startable-component\" that can be controlled via a core.async channel.
+    This generic component is used to define a \"startable-clock-component\". "]
 
    [:article
     [:h3 "Vigiglobe API"]
@@ -35,4 +34,5 @@
       [:li "The third presents the same data as the second but with a different representation. "]]
      ]
     [:section
-     "I reused the startable-component defined in the Test-3 tp produce a generic \"startable-graph-component\". To produce a new graph the important part is to create a function that crunches the data returned by the API and mutates the Chart accordingly."]]])
+     "I reused the startable-component defined in the Test-3 tp produce a generic \"startable-graph-component\" in order to start/pause the live data feed
+      To produce a new graph the important part is to create a function that crunches the data returned by the API and mutates the Chart accordingly."]]])
